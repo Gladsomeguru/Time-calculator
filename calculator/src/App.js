75ca293636965bcce17dcbd33e661847487688ba
Gurunathan <gurunathan.p@indiumsoft.com>
2025-09-28 18:28:33 +0530

    initial commit

---
+++ calculator/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import Calculator from './Calculator';
+
+function App() {
+  return (
+    <div>
+      <Calculator></Calculator>
+    </div>
+  );
+}
+
+export default App;
---
+++ calculator/src/Calculator.js
@@ -0,0 +1,15 @@
+function Calculator() {
+    return (
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-4 min-vh-100 rounded-2 border-2 border">
+                    
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+
+export default Calculator;
