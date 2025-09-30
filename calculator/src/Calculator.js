12b7b2a1763395397036b38e502f6918135437ef
Gurunathan <gurunathan.p@indiumsoft.com>
2025-09-30 23:28:27 +0530

    added keyvalues

--- calculator/src/Calculator.js
+++ calculator/src/Calculator.js
@@ -1,4 +1,24 @@
+import { useState } from "react";
+
+const buttons = [
+    "AC", "C", "%", "/",
+    "7", "8", "9", "*",
+    "4", "5", "6", "-",
+    "1", "2", "3","+", "0", "00", "."
+];
+
+
 function Calculator() {
+    const [num, setNum] = useState("0");
+    const onHandler = (value) => {
+        if (value === "AC") {
+            setNum("0");
+        }else if(value === "C"){
+            setNum(()=>num.slice(0,-1)||"0")
+        } else {
+            setNum(() => (num != '0' ? num + value : value));
+        }
+    }
     return (
         <div className="container">
             <div className="row justify-content-center min-vh-100">
@@ -8,65 +28,30 @@ function Calculator() {
                         Simple and fast calculations to save your time, perform accurate results instantly, and provide a smooth user experience.
                     </span>
                 </div>
-                <div className="calculator col-md-4 rounded-4 my-auto">
+                <div className="calculator col-md-4 col-10 rounded-4 my-auto">
                     <div className="container-fluid">
                         <div className="row justify-content-center g-4 h-100">
-                            <div className="col-12 pt-4 mt-0">
+                            <div className="col-12 pt-4 mt-0 mb-3">
                                 <form>
                                     <div class="form-group px-0">
                                         <label for="output"></label>
                                         <input type="text"
-                                            class="form-control fs-1 output-box text-end" name="output" id="" aria-describedby="helpId" placeholder="" />
+                                            class="form-control fs-1 output-box text-end" name="output" id="output" aria-describedby="helpId" value={num} readOnly/>
                                     </div>
                                 </form>
                             </div>
+                            {
+                                buttons.map((value, index) => {
+                                    return (
+                                        <div className="col-3 rounded-3" key={index}>
+                                            <button className="calc-button rounded-2 fs-6 p-2 fw-medium w-100"
+                                                onClick={() => onHandler(value)}><span>{value}</span></button>
+                                        </div>
+                                    )
+                                })
+                            }
                             <div className="col-3 rounded-3">
-                                <button className="calc-button rounded-2 fs-6 p-3 fw-medium w-100"><span>C</span></button>
-                            </div>
-                            <div className="col-3 rounded-3">
-                                <button className="calc-button rounded-2 fs-6 p-3 fw-medium w-100"><span>CE</span></button>
-                            </div>
-                            <div className="col-3 rounded-3">
-                                <button className="calc-button rounded-2 fs-6 p-3 fw-medium w-100"><span>/</span></button>
-                            </div>
-                            <div className="col-3 rounded-3">
-                                <button className="calc-button rounded-2 fs-6 p-3 fw-medium w-100"><span>*</span></button>
-                            </div>
-                            <div className="col-3 rounded-3">
-                                <button className="calc-button rounded-2 fs-6 p-3 fw-medium w-100"><span>7</span></button>
-                            </div>
-                            <div className="col-3 rounded-3">
-                                <button className="calc-button rounded-2 fs-6 p-3 fw-medium w-100"><span>8</span></button>
-                            </div>
-                            <div className="col-3 rounded-3">
-                                <button className="calc-button rounded-2 fs-6 p-3 fw-medium w-100"><span>9</span></button>
-                            </div>
-                            <div className="col-3 rounded-3">
-                                <button className="calc-button rounded-2 fs-6 p-3 fw-medium w-100"><span>-</span></button>
-                            </div>
-                            <div className="col-3 rounded-3">
-                                <button className="calc-button rounded-2 fs-6 p-3 fw-medium w-100"><span>4</span></button>
-                            </div>
-                            <div className="col-3 rounded-3">
-                                <button className="calc-button rounded-2 fs-6 p-3 fw-medium w-100"><span>5</span></button>
-                            </div>
-                            <div className="col-3 rounded-3">
-                                <button className="calc-button rounded-2 fs-6 p-3 fw-medium w-100"><span>6</span></button>
-                            </div>
-                            <div className="col-3 rounded-3">
-                                <button className="calc-button rounded-2 fs-6 p-3 fw-medium w-100"><span>+</span></button>
-                            </div>
-                            <div className="col-3 rounded-3">
-                                <button className="calc-button rounded-2 fs-6 p-3 fw-medium w-100"><span>1</span></button>
-                            </div>
-                            <div className="col-3 rounded-3">
-                                <button className="calc-button rounded-2 fs-6 p-3 fw-medium w-100"><span>2</span></button>
-                            </div>
-                            <div className="col-3 rounded-3">
-                                <button className="calc-button rounded-2 fs-6 p-3 fw-medium w-100"><span>3</span></button>
-                            </div>
-                            <div className="col-3 rounded-3">
-                                <button className="calc-button equal rounded-2 p-3 fw-medium w-100"><span>=</span></button>
+                                <button className="calc-button equal rounded-2 p-2 fs-6 fw-medium w-100"><span>=</span></button>
                             </div>
                         </div>
                     </div>
