a71955565142cf070e4b53778689b38c9563e01a
Gurunathan <gurunathan.p@indiumsoft.com>
2025-10-08 00:31:03 +0530

    added backcover and updated

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.min.css";
 
 import Calculator from './Calculator';
 
--- calculator/src/Calculator.js
+++ calculator/src/Calculator.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import audio from "./assets/click.mp3";
 
 const buttons = [
-    "AC", "C", "%", "/",
+    "AC", "bi bi-backspace", "%", "/",
     "7", "8", "9", "*",
     "4", "5", "6", "-",
     "1", "2", "3", "+", "0", "00", "."
@@ -11,13 +11,15 @@ const buttons = [
 function Calculator() {
     const [num, setNum] = useState("0");
     const [calculation, setCalculation] = useState("");
+    const [turned, setTurned] = useState(false);
+
     const onHandler = (value) => {
         const click = new Audio(audio);
-        click.play().catch(() => {});
+        click.play().catch(() => { });
         if (value === "AC") {
             setNum("0");
             setCalculation("");
-        } else if (value === "C") {
+        } else if (value === "bi bi-backspace") {
             if (calculation !== "") {
                 setCalculation("");
                 setNum("0");
@@ -31,24 +33,33 @@ function Calculator() {
             } catch (error) {
                 setNum("Error"); // handle invalid expressions
             }
-        } else if ((value === "0" || value === "00" || ["+", "%", "/", "*", "-"].includes(value)) && (num === "0" || /[+\-*./]$/.test(num))) {
+        } else if ((value === "0" || value === "00" || ["+", "%", "/", "*", "-"].includes(value)) && (num === "0" || /[+\-*/]$/.test(num))) {
             return;
         } else {
             setNum((num === "0" || num === "Error") ? value : num + value);
+            if (calculation !== "") {
+                setCalculation("");
+            }
         }
     };
+
+    const turnCalc = () => {
+        setTurned(!turned);
+    }
 
     return (
         <div className="container">
             <div className="row justify-content-center min-vh-100">
-                <div className="col-md-6 col-11 p-md-0 pe-md-3 p-3 my-md-auto mt-4 text-white">
-                    <h1 className="fw-bold">Simple Calculator</h1>
-                    <span className="block text-sm fs-6 text-gray-300">
-                        Simple and fast calculations to save your time, perform accurate results instantly, and provide a smooth user experience.
-                    </span>
+                <div className="col-md-6 col-11 p-md-0 pe-md-5 p-3 my-md-auto mt-4 text-white">
+                    <h1 className="fw-bold mb-3">Simple Calculator</h1>
+                    <p className="block text-sm fs-6 text-gray-300">
+                        Perform all your calculations with ease! <br />
+                        Whether you need to add, subtract, multiply, or divide, this calculator handles it all effortlessly.
+                    </p>
+                    <button className="btn btn-sm px-4 rounded-5 bg-white text-secondary fw-medium" onClick={turnCalc}>View developer</button>
                 </div>
-                <div className="calculator col-md-4 col-11 rounded-4 my-md-auto pb-4">
-                    <div className="container-fluid">
+                <div className={"calculator col-md-4 col-11 rounded-4 my-md-auto pb-4 position-relative" + (turned ? " turned" : "")}>
+                    <div className="container-fluid frontside">
                         <div className="row justify-content-center g-4">
                             <div className="col-12 pt-4 mt-0 mb-3">
                                 <form>
@@ -65,7 +76,7 @@ function Calculator() {
                                     return (
                                         <div className="col-3 rounded-3" key={index}>
                                             <button className="calc-button rounded-2 fs-6 p-2 fw-medium w-100"
-                                                onClick={() => onHandler(value)}><span>{value}</span></button>
+                                                onClick={() => onHandler(value)}>{index === 1 ? <span><i className={value}></i></span> : <span>{value}</span>}</button>
                                         </div>
                                     )
                                 })
@@ -74,6 +85,17 @@ function Calculator() {
                                 <button className="calc-button equal rounded-2 p-2 fs-6 fw-medium w-100" onClick={() => onHandler("=")} ><span>=</span></button>
                             </div>
                         </div>
+                    </div>
+                    <div className="backside position-absolute rounded-4">
+                        <div className="cover mx-auto p-3">
+                            <h6 className="text-center stroked-text">Project-1</h6>
+                            <p className="text-center stroked-text">Developed by Gurunathan
+                            </p>
+                        </div>
+                        <div className="screw one"></div>
+                        <div className="screw two"></div>
+                        <div className="screw three"></div>
+                        <div className="screw four"></div>
                     </div>
                 </div>
             </div>
